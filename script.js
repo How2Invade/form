d65ab4ad0c08e2d7b68ada8fa3bcc46d341ae96c
Meet Mangaonkar <meetmangaonkar@gmail.com>
2026-02-24 01:47:31 +0530

    css styling and js logic change

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 let contacts = JSON.parse(localStorage.getItem("contacts")) || [];
 let editIndex = -1;
 
-displayContacts();
-
 function addOrUpdateContact() {
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let phone = document.getElementById("phone").value;
 
-  // Validation
   if(name === "" || email === "" || phone === ""){
     alert("All fields are required");
     return;
@@ -17,21 +14,21 @@ function addOrUpdateContact() {
   let contact = { name: name, email: email, phone: phone };
 
   if(editIndex === -1){
-    contacts.push(contact);   // ADD NEW CONTACT
+      contacts.push(contact);          // ADD
   } else {
-    contacts[editIndex] = contact;  // UPDATE EXISTING CONTACT
-    editIndex = -1;
+      contacts[editIndex] = contact;   // EDIT
+      editIndex = -1;
   }
 
-  saveContacts();
+  localStorage.setItem("contacts", JSON.stringify(contacts)); // SAVE
   clearForm();
   displayContacts();
 }
 
-function displayContacts(){
+function displayContacts() {
   document.getElementById("contactList").innerHTML = "";
 
-  for(let i = 0; i < contacts.length; i++){
+  for(let i=0; i < contacts.length; i++){
     let c = contacts[i];
 
     let box = document.createElement("div");
@@ -41,35 +38,32 @@ function displayContacts(){
       "<b>Name:</b> " + c.name + "<br>" +
       "<b>Email:</b> " + c.email + "<br>" +
       "<b>Phone:</b> " + c.phone + "<br><br>" +
-      "<button onclick='editContact("+ i +")'>Edit</button> " +
-      "<button onclick='deleteContact("+ i +")'>Delete</button>";
+      "<button onclick='editContact("+i+")'>Edit</button> " +
+      "<button onclick='deleteContact("+i+")'>Delete</button>";
 
     document.getElementById("contactList").appendChild(box);
   }
 }
 
-function editContact(index){
-  let c = contacts[index];
-
+function editContact(i){
+  let c = contacts[i];
   document.getElementById("name").value = c.name;
   document.getElementById("email").value = c.email;
   document.getElementById("phone").value = c.phone;
 
-  editIndex = index;
+  editIndex = i;
 }
 
-function deleteContact(index){
-  contacts.splice(index, 1);
-  saveContacts();
+function deleteContact(i){
+  contacts.splice(i, 1);
+  localStorage.setItem("contacts", JSON.stringify(contacts)); // SAVE AFTER DELETE
   displayContacts();
 }
 
-function saveContacts(){
-  localStorage.setItem("contacts", JSON.stringify(contacts));
-}
-    
 function clearForm(){
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
   document.getElementById("phone").value = "";
 }
+
+displayContacts();  // SHOW CONTACTS ON PAGE LOAD
